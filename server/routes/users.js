2413e2d72cf704cf04cb1c1b46547c0fcbc3c0c2
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-05-07 15:24:15 +0530

    users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,18 +7,12 @@ const {
 } = require("../controllers/userController");
 
 // @route   GET /api/users/me
-// @desc    Get current user
-// @access  Private
 router.get("/me", getCurrentUser);
 
 // @route   GET /api/users/profile
-// @desc    Get user profile
-// @access  Private
 router.get("/profile", getUserProfile);
 
 // @route   PUT /api/users/profile
-// @desc    Update user profile
-// @access  Private
 router.put("/profile", updateProfile);
 
 module.exports = router;
