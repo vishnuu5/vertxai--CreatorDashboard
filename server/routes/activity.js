5b038fc5446757e4b36d36c1f792c2dcc7a4a019
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-05-07 15:20:48 +0530

    activity.js

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getRecentActivity } = require("../controllers/activityController");
 
-// @route   GET /api/activity/recent
-// @desc    Get recent activity
-// @access  Private
+// @route   GET /api/activity/recen
 router.get("/recent", getRecentActivity);
 
 module.exports = router;
